3c8481a41be974a80cb2f5c6155538cf6db253e2
Alexander Rykhlitskiy <alexander.rykhlitskiy@gmail.com>
2016-11-15 20:56:49 +0300

    Add youtube videous support

--- README.md
+++ README.md
@@ -1,10 +1,16 @@
 With this Coolness Measurement Tool you can measure coolness of audio parts you specify.
 
-To use this tool you need [sox](http://sox.sourceforge.net/) to be installed.
+To use this tool you need [sox](http://sox.sourceforge.net/) to be installed. To download audio from YouTube you also need [youtube-dl](https://rg3.github.io/youtube-dl/)
 ## Usage example
+Providing youtube url
+```bash
+ruby cmt.rb https://www.youtube.com/watch\?v\=ICz-edsOaFw 2:46-3:40 3:40-4:30 4:30-5:17 5:17-6:06 6:06-7:00 7:00-7:30 7:30-7:50 8:04-8:32 8:32-8:59 8:59-9:35
+```
+or audio file path
 ```bash
 ruby cmt.rb './samples/Финал Линди Хоп JnJ Open часть 1.mp3' 2:46-3:40 3:40-4:30 4:30-5:17 5:17-6:06 6:06-7:00 7:00-7:30 7:30-7:50 8:04-8:32 8:32-8:59 8:59-9:35
 ```
+
 will give you
 ```
     period      coolness
--- cmt.rb
+++ cmt.rb
@@ -1,11 +1,26 @@
 require 'fileutils'
+require 'uri'
 
-audio_file = ARGV[0]
+file_path = ARGV[0]
 time_periods = ARGV[1..-1]
+
+if file_path =~ URI::regexp
+  available_formats = `youtube-dl -F #{file_path}`
+  target_format = available_formats.split("\n").slice_after { |s| s.start_with?('format') }.to_a.last.first.split.first
+  download_log = `youtube-dl -f #{target_format} #{file_path}`
+  destination_marker = '[download] Destination: '
+  audio_file_path = download_log.split("\n").find { |s| s.start_with?(destination_marker) }.gsub(destination_marker, '')
+  new_audio_file_path = audio_file_path + '.mp3'
+  `ffmpeg -v 5 -y -i #{audio_file_path} -acodec libmp3lame -ac 2 -ab 192k #{new_audio_file_path}`
+  audio_file_path = new_audio_file_path
+end
+
+audio_file_path ||= file_path
+
 periods_and_coolness = time_periods.map do |period|
   start, ent = period.split('-')
-  file_name = "#{audio_file}_#{period}.mp3"
-  `sox '#{audio_file}' '#{file_name}' trim #{start} '=#{ent}'`
+  file_name = "#{audio_file_path}_#{period}.mp3"
+  `sox '#{audio_file_path}' '#{file_name}' trim #{start} '=#{ent}'`
 
   stat = `sox '#{file_name}' -n stat 2>&1`
   FileUtils.rm(file_name)
